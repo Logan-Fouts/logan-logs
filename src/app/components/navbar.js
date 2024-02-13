293484af59d8f32ae4c442dcb6624bed0bde3e59
Logan-Fouts <loganfouts@proton.me>
2024-02-14 00:19:00 +0100

    few adjustments

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -2,9 +2,9 @@ import "./../globals.css";
 
 export default function Navbar() {
   return (
-    <div className="flex flex-col min-h-screen w-full">
-      <nav className="bg-black text-white flex flex-col flex-grow items-center w-2/3">
-        <img src="./../favicon.ico" className="w-1/2 my-4" />
+    <div className="flex flex-col sm:flex-row min-h-screen sm:min-h-0 w-full">
+      <nav className="bg-black text-white flex flex-col sm:flex-row flex-grow items-center w-2/3">
+        <img src="./../favicon.ico" className="my-4 sm: my-0 self-start" /> 
         <a href="/documentation" className="my-4 text-lg self-start pl-4">
           Documentationˇ
         </a>
