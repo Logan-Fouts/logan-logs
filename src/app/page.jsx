7eb7abfd86984335eea9264f15ad832274a454da
lf222yv <lf222yv@student.lnu.se>
2024-02-15 17:03:01 +0100

    landing page and work on docs page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,14 +3,39 @@ import Navbar from "./components/NavBar.client";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-screen h-screen ">
+    <div className="flex flex-col w-screen h-screen justify-center items-center">
       <Navbar />
-      <div className="flex flex-col w-full h-4/5 justify-around mt-16 ml-16">
-        <h1 className="font-black text-6xl w-4/5">Welcome To Logan Logs</h1>
-        <h2 className="w-3/5">
+      <div className="flex w-full h-5/5 flex-col justify-center">
+        <div className="flex flex-row items-center justify-center">
+          <h1 className="font-black text-5xl ml-4">Welcome To Logan Logs</h1>
+          <img src="/logo.png" alt="Logan Logs Logo" className="h-40"></img>
+        </div>
+        <h2 className="mt-4 text-center">
           Here I compile software and tech knowledge and experiences.
         </h2>
-        <a href="https://github.com/Logan-Fouts">My GitHub</a>
+        <div className="text-center mt-12">
+          <h1 className="font-black text-green-800 text-3xl mb-2">My Links</h1>
+          <ul className="list-none list-disc text-white">
+            <li>
+              <a
+                href="https://github.com/Logan-Fouts"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://se.linkedin.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Linkedin
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/pages/documentation.jsx
+++ src/pages/documentation.jsx
@@ -1,10 +1,26 @@
+import React, { useState, useEffect } from "react";
+import MarkdownRenderer from "./../app/components/MarkDownRenderer";
 import Navbar from "/src/app/components/NavBar.client";
+import "./../app/globals.css";
 
 export default function Documentation() {
+  const [markdown, setMarkdown] = useState("");
+
+  useEffect(() => {
+    fetch("/markdown.md")
+      .then((response) => response.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
   return (
-    <div className="flex flex-col w-screen h-screen justify-center items-center">
+    <div className="flex flex-col w-screen min-h-screen justify-center items-center mb-12 p-2">
       <Navbar />
-      <h1 className="font-black">Documentation Page</h1>
+      <div className="bg-white mt-16 w-4/5 rounded-md p-2">
+        <MarkdownRenderer
+          markdown={markdown}
+          className="flex flex-col items-center justify-center w-4/5 h-4/5 prose mx-auto p-2"
+        />
+      </div>
     </div>
   );
 }
