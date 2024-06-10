57f381b29bd789ea4575d8ce9c2278a162222051
Logan-Fouts <loganfouts@proton.me>
2024-06-10 14:25:54 +0200

    add: more responsive

--- src/app/components/Project.jsx
+++ src/app/components/Project.jsx
@@ -11,28 +11,32 @@ const Project = ({
   linkUrl,
 }) => {
   return (
-    <div className="z-30 text-center max-full w-3/4 max-w-6xl p-5 bg-white rounded-lg shadow-lg mt-8 mb-8 dark:bg-gray-800 dark:text-white">
-      <a href={linkUrl} target="_blank" rel="noopener noreferrer">
-        <h1 className="font-black text-blue-500 text-2xl mb-4 underline">
-          {title}
-        </h1>
-      </a>
-      <Image
-        src={imgSrc}
-        width={1000}
-        height={300}
-        alt={title}
-        className="w-full h-96 object-cover object-center rounded-lg"
-      />
-      <div className="p-5 dark:text-white">
-        <h2 className="text-black text-xl font-black dark:text-white">
-          {heading1}
-        </h2>
-        <p className="text-gray-700 mb-4 dark:text-white">{intro}</p>
-        <h2 className="text-black text-xl font-black dark:text-white">
-          {heading2}
-        </h2>
-        <p className="text-gray-600 dark:text-white">{mainText}</p>
+    <div className="p-4 max-h-svh z-30 flex flex-col md:flex-row max-w-6xl rounded-lg shadow-lg overflow-hidden mt-8 mb-8 mx-auto w-11/12 dark:bg-gray-800 dark:text-white">
+      <div className="md:w-3/5 md:h-auto h-1/4">
+        <Image
+          src={imgSrc}
+          width={700}
+          height={700}
+          alt={`${title} - Image`}
+          className="w-full h-full object-cover rounded-l-lg"
+        />
+      </div>
+      <div className="hover:scrollbar-thumb-[#1f2836] active:scrollbar-thumb-sky-400 scrollbar scrollbar-thumb-[#1f2836] scrollbar-track-slate-700 overflow-y-scroll md:w-2/5 flex flex-col justify-between">
+        <div className="p-8">
+          <a href={linkUrl} target="_blank" rel="noopener noreferrer">
+            <h1 className="font-black text-blue-500 text-2xl mb-4 underline">
+              {title}
+            </h1>
+          </a>
+          <h2 className="text-black text-xl font-black mb-2 dark:text-white">
+            {heading1}
+          </h2>
+          <p className="text-gray-700 mb-4 dark:text-white">{intro}</p>
+          <h2 className="text-black text-xl font-black mb-2 dark:text-white">
+            {heading2}
+          </h2>
+          <p className="text-gray-600 dark:text-white">{mainText}</p>
+        </div>
       </div>
     </div>
   );
@@ -48,29 +52,27 @@ const Project2 = ({
   linkUrl,
 }) => {
   return (
-    <div className="z-30 flex flex-col max-w-6xl md:flex-row bg-white rounded-lg shadow-lg overflow-hidden p-2 mt-8 mb-8 mx-auto w-3/4 dark:bg-gray-800 dark:text-white">
-      <div className="md:w-1/2 dark:text-white">
+    <div className="p-4 max-h-svh z-30 flex flex-col md:flex-row max-w-6xl rounded-lg shadow-lg overflow-hidden mx-auto mt-8 mb-8 w-11/12 dark:bg-gray-800 dark:text-white">
+      <div className="md:w-3/5 md:h-auto h-1/4">
         <Image
           src={imgSrc}
           width={700}
           height={700}
-          alt={title}
+          alt={`${title} - Image`}
           className="w-full h-full object-cover rounded-l-lg"
         />
       </div>
-      <div className="md:w-1/2 p-8 dark:text-white">
-        <a href={linkUrl} target="_blank" rel="noopener noreferrer">
-          <h1 className="font-black text-blue-500 text-2xl mb-4 underline">
-            {title}
-          </h1>
-        </a>
-        <div className="mb-6 dark:text-white">
+      <div className="hover:scrollbar-thumb-[#1f2836] active:scrollbar-thumb-sky-400 scrollbar scrollbar-thumb-[#1f2836] scrollbar-track-slate-700 overflow-y-scroll md:w-2/5 flex flex-col justify-between">
+        <div className="p-8">
+          <a href={linkUrl} target="_blank" rel="noopener noreferrer">
+            <h1 className="font-black text-blue-500 text-2xl mb-4 underline">
+              {title}
+            </h1>
+          </a>
           <h2 className="text-black text-xl font-black mb-2 dark:text-white">
             {heading1}
           </h2>
-          <p className="text-gray-700 dark:text-white">{intro}</p>
-        </div>
-        <div>
+          <p className="text-gray-700 mb-4 dark:text-white">{intro}</p>
           <h2 className="text-black text-xl font-black mb-2 dark:text-white">
             {heading2}
           </h2>
@@ -93,17 +95,17 @@ const Project3 = ({
   linkUrl,
 }) => {
   return (
-    <div className="z-30 flex flex-col md:flex-row max-w-6xl rounded-lg shadow-lg overflow-hidden mt-8 mb-8 mx-auto w-3/4 dark:bg-gray-800 dark:text-white">
-      <div className="md:w-3/5 dark:text-white">
+    <div className="p-4 max-h-svh z-30 flex flex-col md:flex-row max-w-6xl rounded-lg shadow-lg overflow-hidden mx-auto mt-8 mb-8 w-11/12 dark:bg-gray-800 dark:text-white">
+      <div className="md:w-3/5 md:h-auto h-1/4">
         <Image
           src={imgSrc1}
-          width={700} // Adjusted for a larger display of the main image
+          width={700}
           height={700}
           alt={`${title} - Image 1`}
           className="w-full h-full object-cover rounded-l-lg"
         />
       </div>
-      <div className="md:w-2/5 flex flex-col justify-between dark:text-white">
+      <div className="hover:scrollbar-thumb-[#1f2836] active:scrollbar-thumb-sky-400 scrollbar scrollbar-thumb-[#1f2836] scrollbar-track-slate-700 overflow-y-scroll md:w-2/5 flex flex-col justify-between">
         <div className="p-8">
           <a href={linkUrl} target="_blank" rel="noopener noreferrer">
             <h1 className="font-black text-blue-500 text-2xl mb-4 underline">
@@ -119,7 +121,7 @@ const Project3 = ({
           </h2>
           <p className="text-gray-600 dark:text-white">{mainText}</p>
         </div>
-        <div className="p-4 md:p-8">
+        <div className="p-8">
           <Image
             src={imgSrc2}
             width={300}
@@ -131,7 +133,7 @@ const Project3 = ({
             src={imgSrc3}
             width={300}
             height={300}
-            alt={`${title} - Image 2`}
+            alt={`${title} - Image 3`}
             className="object-cover rounded-lg"
           />
         </div>
