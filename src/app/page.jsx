65e867bc10125fc856931c3858a02f51e51612b6
Logan-Fouts <loganfouts@proton.me>
2024-08-21 13:43:08 +0200

    add: picture of myself

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,54 +19,53 @@ export default function Home() {
           transition={{ duration: 0.8 }}
           className="text-center"
         >
-          <div className="flex items-center justify-center mb-6">
+          <div className="md:translate-x-20 flex items-center justify-center mb-6">
             <h1 className="font-black text-4xl sm:text-6xl mr-4 bg-clip-text text-green-500 p-4">
               Welcome To Logan Logs
+              <p className="font-normal text-white pt-5 text-lg sm:text-xl mb-12 max-w-2xl mx-auto">
+                Here I compile my software/tech knowledge and experiences.
+              </p>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.5, duration: 0.8 }}
+              >
+                <h2 className="font-bold text-2xl sm:text-3xl mb-6 text-green-400">
+                  Socials
+                </h2>
+                <div className="flex justify-center space-x-6 text-white">
+                  <SocialLink
+                    href="https://github.com/Logan-Fouts"
+                    icon={<FaGithub className="w-8 h-8" />}
+                    label="GitHub"
+                  />
+                  <SocialLink
+                    href="https://www.linkedin.com/in/logan-fouts-aab33b230/"
+                    icon={<FaLinkedin className="w-8 h-8" />}
+                    label="LinkedIn"
+                  />
+                </div>
+              </motion.div>
             </h1>
             <motion.div
               initial={{ rotate: 0 }}
-              animate={{ rotate: [-30, 30] }}
+              animate={{ rotate: [-5, 5] }}
               transition={{
-                duration: 15,
+                duration: 10,
                 repeat: Infinity,
                 repeatType: "reverse",
                 ease: "easeInOut",
               }}
+              className="hidden md:block"
             >
               <Image
-                src="/logo.png"
-                width={80}
-                height={80}
+                src="/jag2.png"
+                width={500}
+                height={500}
                 alt="Logan Logs Logo"
               />
             </motion.div>
           </div>
-
-          <p className="text-lg sm:text-xl mb-12 max-w-2xl mx-auto">
-            Here I compile my software/tech knowledge and experiences.
-          </p>
-
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.5, duration: 0.8 }}
-          >
-            <h2 className="font-bold text-2xl sm:text-3xl mb-6 text-green-400">
-              Socials
-            </h2>
-            <div className="flex justify-center space-x-6">
-              <SocialLink
-                href="https://github.com/Logan-Fouts"
-                icon={<FaGithub className="w-8 h-8" />}
-                label="GitHub"
-              />
-              <SocialLink
-                href="https://www.linkedin.com/in/logan-fouts-aab33b230/"
-                icon={<FaLinkedin className="w-8 h-8" />}
-                label="LinkedIn"
-              />
-            </div>
-          </motion.div>
         </motion.div>
       </main>
     </div>
